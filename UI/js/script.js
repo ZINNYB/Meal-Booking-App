81ea1cd83b03c4e981587bb375957a04e758ff08
ezinne kanu <ezinneelizabeth8814@yaooh.com>
2019-02-09 23:07:02 +0100

    added a meal option/choice section to the food menu

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,13 +1,15 @@
-
-const form = document.getElementById('form-container');
+ 
+const form = document.getElementById('details-case');
 
 const btn = document.getElementById('form-cancel-btn');
-
-const formCase =document.getElementsByClassName('allforms-bg')[0];
-const mealCase = document.querySelectorAll('.process-case')[0]
-
+const mealCase = document.querySelectorAll('.process-case')
+for (let i = 0; i < mealCase.length; i++) {
+    mealCase[i].addEventListener('click', ()=>{
+        form.style.display='block'; });
+}
+ 
 const closeBtn =  ()=>{
-   return form.style.display='none'; 
+    form.style.display='none'; 
 }
 
 const closeForm = (event) => {
@@ -15,10 +17,6 @@ const closeForm = (event) => {
         form.style.display = "none";
     }
 }
-const foodInfo = ()=>{
-return formCase.style.display='block';
-}
 
-mealCase.addEventListener('click', foodInfo );
 window.addEventListener('click', closeForm);
 btn. addEventListener("click",  closeBtn);
