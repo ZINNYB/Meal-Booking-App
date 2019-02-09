4c7b46c85e3632feab5ef5f2d4333fe640fb775b
ezinne kanu <ezinneelizabeth8814@yaooh.com>
2019-02-09 12:19:36 +0100

    further changes to profile

--- UI/js/script.js
+++ UI/js/script.js
@@ -4,19 +4,20 @@ const form = document.getElementById('form-container');
 const btn = document.getElementById('form-cancel-btn');
 
 const formCase =document.getElementsByClassName('allforms-bg')[0];
-const mealCase = document.querySelectorAll('.process-case')[0]
+const mealCase = document.getElementsByClassName('process-case')[0]
 
-const closeBtn =  ()=>{
+ const closeBtn =  ()=>{
    return form.style.display='none'; 
-}
+ }
 
-const closeForm = (event) => {
+    const closeForm = (event) => {
     if (event.target == form) {
-        form.style.display = "none";
-    }
+       form.style.display = "none";
+   }
 }
-const foodInfo = ()=>{
-return formCase.style.display='block';
+const foodInfo = (event)=>{
+    if( event.target === mealCase )
+return formCase.style.display='btn';
 }
 
 mealCase.addEventListener('click', foodInfo );
