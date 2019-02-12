aa529c35f7cd8bff5a1c57ea3bd7da62a36873ac
ezinne kanu <ezinneelizabeth8814@yaooh.com>
2019-02-12 12:29:14 +0100

    modified the menu bars of the html file to enable resizing of small screens

--- UI/js/script.js
+++ UI/js/script.js
@@ -10,16 +10,22 @@ const formCase =document.getElementsByClassName('allforms-bg')[0];
 // const btn = document.getElementById('form-cancel-btn');
 // const mealCase = document.querySelectorAll('.process-case')
 
+  /* Navigation bar */
+ const mainNav = document.querySelector('.js-menu');
+  const navBarToggle = document.querySelector('.js-navbar-toggle');
+  
+  const toggleMenu =  () =>{
+    mainNav.classList.toggle('active');
+  }
 
+    //  my modal box
 for (let i = 0; i < mealCase.length; i++) {
     mealCase[i].addEventListener('click', ()=>{
     form.style.display='block';});
 }
-
 const closeBtn =  ()=>{
 	return form.style.display='none'; 
 }
-
 const closeForm = (event) => {
 	if (event.target == form) {
 	   form.style.display = "none";
@@ -41,7 +47,7 @@ if (btn) {
 }
 
 
-/* Modal Box */
+    /* Modal Box */
 const modalBox = function(modalBox, btnModalBoxOpen, btnModalBoxClose) {
 
   if (btnModalBoxOpen) {
@@ -63,3 +69,4 @@ const loginFormWrapper = document.getElementById('login-form-wrapper');
 const loginOpen = document.getElementById('login-open');
 const loginClose = document.getElementById('login-close');
 modalBox(loginFormWrapper, loginOpen, loginClose);
+navBarToggle.addEventListener('click', toggleMenu);
